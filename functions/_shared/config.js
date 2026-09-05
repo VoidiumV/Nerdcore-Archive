3af9a7045940a54e60f3d35294dbba2c35ed1453
Voidium <rogers.brothers.games@gmail.com>
2026-09-05 16:11:15 -0400

    Create config.js

---
+++ functions/_shared/config.js
@@ -0,0 +1,4 @@
+// functions/_shared/config.js
+
+export const SITE_URL = 'https://nerdcore-archive.pages.dev';
+export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzBTKdP2LuVyuRMWe1uX6OUzakK88Bfs8x6HvhnZQ-o5ZjU6okVY26KOiNUuFJD8o8w/exec';
