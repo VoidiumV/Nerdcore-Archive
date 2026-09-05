74a047d38a7a2d758568552c46ba2cb38a094783
Voidium <rogers.brothers.games@gmail.com>
2026-09-05 16:22:30 -0400

    Update config.js

--- functions/_shared/config.js
+++ functions/_shared/config.js
@@ -1,4 +1,4 @@
 // functions/_shared/config.js
 
 export const SITE_URL = 'https://nerdcore-archive.pages.dev';
-export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzBTKdP2LuVyuRMWe1uX6OUzakK88Bfs8x6HvhnZQ-o5ZjU6okVY26KOiNUuFJD8o8w/exec';
+export const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbz-uGu3wNnC3B_BXrIG9-qpf6wVE6D8rFuDVqRuFUtL39rPJZvv2l-MA0o0M6gcA3vl/exec';
