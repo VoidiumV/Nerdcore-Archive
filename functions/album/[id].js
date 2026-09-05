e26cfda90a5c4abfc33ec36cf67187d62056e0a6
Voidium <rogers.brothers.games@gmail.com>
2026-09-05 16:46:00 -0400

    Update [id].js

--- functions/album/[id].js
+++ functions/album/[id].js
@@ -11,7 +11,7 @@ export async function onRequest(context) {
   const shareUrl = SITE_URL + '/album/' + encodeURIComponent(id);
 
   let title = 'Nerdcore Archive';
-  let description = "BritishJuggernaut's Unofficial Nerdcore Site";
+  let description = "BritishJuggernaut's Nerdcore Archive";
   let image = '';
 
   try {
