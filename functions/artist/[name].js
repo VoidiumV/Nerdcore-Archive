6b7294df39bcf7c838505c500460b08590685136
Voidium <rogers.brothers.games@gmail.com>
2026-09-05 16:17:16 -0400

    Update [name].js

--- functions/artist/[name].js
+++ functions/artist/[name].js
@@ -35,10 +35,11 @@ export async function onRequest(context) {
     // Fall back to the generic card below.
   }
 
+  const found = !!image;
   return new Response(buildEmbedHtml_(title, description, image, shareUrl, hashUrl, 'profile'), {
     headers: {
       'content-type': 'text/html; charset=UTF-8',
-      'cache-control': 'public, max-age=300'
+      'cache-control': found ? 'public, max-age=300' : 'no-store'
     }
   });
 }
